9bce247483d2bb07d76ce6a175719b482b38a727
hex <786019030@qq.com>
2021-03-06 02:04:53 +0800

    typescript 命名空间与模块整理

---
+++ drag-typescript/src/components/project-input.ts
@@ -0,0 +1,86 @@
+/// <reference path="base-component.ts"/>
+/// <reference path="../decoreators/autobing.ts"/>
+/// <reference path="../util/validation.ts"/>
+/// <reference path="../state/project-state.ts"/>
+
+namespace App {
+    export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+        titleInputElement: HTMLInputElement;
+        descriptionInputElement: HTMLInputElement;
+        peopleInputElement: HTMLInputElement;
+
+        constructor() {
+            super('project-input', 'app', true, 'user-input');
+
+            this.titleInputElement = this.element.querySelector(
+                '#title'
+            ) as HTMLInputElement;
+            this.descriptionInputElement = this.element.querySelector(
+                '#description'
+            ) as HTMLInputElement;
+            this.peopleInputElement = this.element.querySelector(
+                '#people'
+            ) as HTMLInputElement;
+
+            this.configure();
+        }
+
+        configure() {
+            this.element.addEventListener('submit', this.submitHandler);
+        }
+        renderContent(): void {
+            throw new Error('Method not implemented.');
+        }
+
+        private gatherUserInput(): [string, string, number] | void {
+            const enteredTitle = this.titleInputElement.value;
+            const enteredDescripttion = this.descriptionInputElement.value;
+            const enteredPeople = this.peopleInputElement.value;
+
+            const titleValidatable: Validation = {
+                value: enteredTitle,
+                required: true,
+            };
+
+            const descriptionValidatable: Validation = {
+                value: enteredDescripttion,
+                required: true,
+                minLength: 5,
+            };
+            const peopleValidatable: Validation = {
+                value: +enteredPeople,
+                required: true,
+                min: 1,
+                max: 5,
+            };
+
+            if (
+                !validate(titleValidatable) ||
+                !validate(descriptionValidatable) ||
+                !validate(peopleValidatable)
+            ) {
+                alert('Invalid input, please try again!');
+                return;
+            } else {
+                return [enteredTitle, enteredDescripttion, +enteredPeople];
+            }
+        }
+
+        private clearInput() {
+            this.titleInputElement.value = '';
+            this.descriptionInputElement.value = '';
+            this.peopleInputElement.value = '';
+        }
+
+        @autobing
+        private submitHandler(event: Event) {
+            event.preventDefault();
+            const userInput = this.gatherUserInput();
+            if (Array.isArray(userInput)) {
+                const [title, desc, people] = userInput;
+                projectState.addProject(title, desc, people);
+                this.clearInput();
+            }
+        }
+    }
+}
